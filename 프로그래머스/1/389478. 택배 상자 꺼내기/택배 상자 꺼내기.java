85696253d6c83d60be08ac2c2a0cf8ea53d5ebd0
minsung gwak <131960511+opinionist@users.noreply.github.com>
2025-03-12 16:57:02 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.02 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n, int w, int num) {
+        int answer = 0;
+        while (num <= n) {
+            num += 2 * (w - ((num - 1) % w) - 1) + 1;
+            answer ++;
+        }
+        return answer;
+    }
+}
